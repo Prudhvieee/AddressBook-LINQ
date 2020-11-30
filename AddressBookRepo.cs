95391c83e73200a061ddc18f85d224d5a6e8e7e4
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-30 07:21:47 +0530

    [Prudhvi] add . able to edit contact

--- AddressBookRepo.cs
+++ AddressBookRepo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Linq;
 using System.Text;
 
 namespace AddressBook_LINQ
@@ -11,7 +12,11 @@ namespace AddressBook_LINQ
         /// uc-1
         /// Creating Data Table
         /// </summary>
-        readonly DataTable dataTable = new DataTable();
+        DataTable dataTable = new DataTable();
+        /// <summary>
+        /// UC-2
+        /// Adding data into datatable
+        /// </summary>
         public void AddData()
         {
             dataTable.Columns.Add("FirstName", typeof(string));
@@ -22,13 +27,43 @@ namespace AddressBook_LINQ
             dataTable.Columns.Add("ZipCode", typeof(string));
             dataTable.Columns.Add("PhoneNumber", typeof(string));
             dataTable.Columns.Add("EmailID", typeof(string));
-
+            ///UC-3
             dataTable.Rows.Add("Krunal", "pandya", "thomas road", "Delhi", "Delhi", "123456", "9876543210", "krunal@yahoo.com");
             dataTable.Rows.Add("Hardik", "pandya", "mount road", "Mumbai", "Maharashtra", "987654", "9123456780", "hardik@gmail.com");
             dataTable.Rows.Add("myanak", "agarwal", "church road", "Bangalore", "Karnataka", "09876", "9121763450", "myank@gmail.com");
             dataTable.Rows.Add("joe", "biden", "arch road", "Saanfransisco", "USA", "143599", "9891283121", "joe@yahoo.com");
             dataTable.Rows.Add("tony", "stark", "north street", "vegas", "USA", "908734", "9872162562", "tony@gmail.com");
             dataTable.Rows.Add("peter", "son", "south street", "los angles", "America", "987152", "098216767231", "peter@gmail.com");
+        }
+        public void DisplayData()
+        {
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (DataColumn col in dataTable.Columns)
+                {
+                    Console.Write(row[col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public void EditContact(string firstName, string lastName, string address, string city, string state, string zipcode, string phoneNumber, string email)
+        {
+            var recordedData = dataTable.AsEnumerable().Where(x => x.Field<string>("FirstName") == firstName).FirstOrDefault();
+            if (recordedData != null)
+            {
+                recordedData.SetField("LastName", lastName);
+                recordedData.SetField("Address", address);
+                recordedData.SetField("City", city);
+                recordedData.SetField("State", state);
+                recordedData.SetField("ZipCode", zipcode);
+                recordedData.SetField("EmailID", email);
+                recordedData.SetField("State", state);
+                Console.WriteLine("Contact edited successfully");
+            }
+            else
+            {
+                Console.WriteLine("No Contact Found");
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,6 +9,25 @@ namespace AddressBook_LINQ
             Console.WriteLine("Welcome to address book");
             AddressBookRepo repo = new AddressBookRepo();
             repo.AddData();
+            repo.DisplayData();
+            Console.WriteLine("Enter the person details to add in the address book");
+            Console.WriteLine("First Name");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("Last Name");
+            string LastName = Console.ReadLine();
+            Console.WriteLine("Address");
+            string Address = Console.ReadLine();
+            Console.WriteLine("City");
+            string City = Console.ReadLine();
+            Console.WriteLine("State");
+            string State = Console.ReadLine();
+            Console.WriteLine("Zip code");
+            string ZipCode = Console.ReadLine();
+            Console.WriteLine("Phone Number");
+            string PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Email");
+            string Email = Console.ReadLine();
+            repo.EditContact(FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, Email);
         }
     }
 }
