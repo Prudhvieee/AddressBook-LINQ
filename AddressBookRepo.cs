2442bd62e9607998d83675889741f39cde4a46fd
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-30 06:52:53 +0530

    [Prudhvi] add . create data table

---
+++ AddressBookRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AddressBook_LINQ
+{
+    public class AddressBookRepo
+    {
+        /// <summary>
+        /// uc-1
+        /// Creating Data Table
+        /// </summary>
+        DataTable dataTable = new DataTable();
+    }
+}
