fdb9510f434c7bc182979c06de1025c8f14a6581
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-11-30 07:05:33 +0530

    [Prudhvi] add . inserting contacts into table

--- AddressBookRepo.cs
+++ AddressBookRepo.cs
@@ -22,6 +22,13 @@ namespace AddressBook_LINQ
             dataTable.Columns.Add("ZipCode", typeof(string));
             dataTable.Columns.Add("PhoneNumber", typeof(string));
             dataTable.Columns.Add("EmailID", typeof(string));
+
+            dataTable.Rows.Add("Krunal", "pandya", "thomas road", "Delhi", "Delhi", "123456", "9876543210", "krunal@yahoo.com");
+            dataTable.Rows.Add("Hardik", "pandya", "mount road", "Mumbai", "Maharashtra", "987654", "9123456780", "hardik@gmail.com");
+            dataTable.Rows.Add("myanak", "agarwal", "church road", "Bangalore", "Karnataka", "09876", "9121763450", "myank@gmail.com");
+            dataTable.Rows.Add("joe", "biden", "arch road", "Saanfransisco", "USA", "143599", "9891283121", "joe@yahoo.com");
+            dataTable.Rows.Add("tony", "stark", "north street", "vegas", "USA", "908734", "9872162562", "tony@gmail.com");
+            dataTable.Rows.Add("peter", "son", "south street", "los angles", "America", "987152", "098216767231", "peter@gmail.com");
         }
     }
 }
